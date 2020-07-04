0fa05e496c310bde8442495205b0977e735fc76e
ZhangZhengHao <46239371+ChingHo97@users.noreply.github.com>
2020-07-04 11:32:06 +0800

    Update augment.py
    fix some bugs

--- dataset/augment.py
+++ dataset/augment.py
@@ -1,20 +1,19 @@
 import torch
-import math
+import math, random
+from PIL import Image
 import random
 import torchvision.transforms as transforms
-import numpy as np
-
 class Transforms(object):
     def __init__(self):
         pass
 
     def __call__(self, img, boxes):
-        if random.random() > 0.5:
+        if random.random() < 0.3:
             img, boxes = colorJitter(img, boxes)
-        if random.random() > 0.5:
+        if random.random() < 0.5:
             img, boxes = random_rotation(img, boxes)
-        #if random.random() < 0.5:
-            #img, boxes = random_crop_resize(img, boxes)
+        if random.random() < 0.5:
+            img, boxes = random_crop_resize(img, boxes)
         return img, boxes
 
 def colorJitter(img, boxes, brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1):
@@ -25,36 +24,40 @@ def colorJitter(img, boxes, brightness=0.1, contrast=0.1, saturation=0.1, hue=0.
 
 
 def random_rotation(img, boxes, degree=10):
-    """
-    :param img: PIL Image
-    :param boxes:  numpy array [N, 4] (x1,y1,x2,y2)
-    :return:
-    """
     d = random.uniform(-degree, degree)
-    w,h = img.size
+    w, h = img.size
     rx0, ry0 = w / 2.0, h / 2.0
     img = img.rotate(d)
-    a =  -d / 180.0 * math.pi
-    new_boxes = np.zeros_like(boxes)
-    new_boxes = torch.from_numpy(new_boxes)
+    a = -d / 180.0 * math.pi
+    boxes = torch.from_numpy(boxes)
+    new_boxes = torch.zeros_like(boxes)
+    new_boxes[:, 0] = boxes[:, 1]
+    new_boxes[:, 1] = boxes[:, 0]
+    new_boxes[:, 2] = boxes[:, 3]
+    new_boxes[:, 3] = boxes[:, 2]
     for i in range(boxes.shape[0]):
-        xmin, ymin, xmax, ymax = boxes[i,:]
-        xmin , ymin, xmax, ymax = float(xmin), float(ymin), float(xmax),float(ymax)
+        ymin, xmin, ymax, xmax = new_boxes[i, :]
+        xmin, ymin, xmax, ymax = float(xmin), float(ymin), float(xmax), float(ymax)
         x0, y0 = xmin, ymin
         x1, y1 = xmin, ymax
         x2, y2 = xmax, ymin
         x3, y3 = xmax, ymax
-        z = torch.FloatTensor([[x0,y0], [x1,y1], [x2,y2], [x3,y3]])
+        z = torch.FloatTensor([[y0, x0], [y1, x1], [y2, x2], [y3, x3]])
         tp = torch.zeros_like(z)
-        tp[:, 0] = (z[:, 0] - rx0) * math.cos(a) - (z[:, 1] - ry0) * math.sin(a) + rx0
-        tp[:, 1] = (z[:, 0] - rx0) * math.sin(a) + (z[:, 1] - ry0) * math.cos(a) + ry0
-        xmin, ymin = torch.min(tp, dim= 0)[0]
-        xmax, ymax = torch.max(tp, dim= 0)[0]
-        new_boxes[i,:] = torch.FloatTensor([xmin, ymin, xmax, ymax])
-    new_boxes[:,[0, 2]].clamp_(min = 0, max = w - 1)
-    new_boxes[:,[1, 3]].clamp_(min = 0, max= h - 1)
-    new_boxes = new_boxes.numpy()
-    return img, new_boxes
+        tp[:, 1] = (z[:, 1] - rx0) * math.cos(a) - (z[:, 0] - ry0) * math.sin(a) + rx0
+        tp[:, 0] = (z[:, 1] - rx0) * math.sin(a) + (z[:, 0] - ry0) * math.cos(a) + ry0
+        ymax, xmax = torch.max(tp, dim=0)[0]
+        ymin, xmin = torch.min(tp, dim=0)[0]
+        new_boxes[i] = torch.stack([ymin, xmin, ymax, xmax])
+    new_boxes[:, 1::2].clamp_(min=0, max=w - 1)
+    new_boxes[:, 0::2].clamp_(min=0, max=h - 1)
+    boxes[:, 0] = new_boxes[:, 1]
+    boxes[:, 1] = new_boxes[:, 0]
+    boxes[:, 2] = new_boxes[:, 3]
+    boxes[:, 3] = new_boxes[:, 2]
+    boxes = boxes.numpy()
+    return img, boxes
+
 
 
 def _box_inter(box1, box2):
